621704cceaaf397761dbd68422b9af2b2a533a5f
Omer Degirmenci <rumidegirmenci0@gmail.com>
2020-10-01 13:46:46 +0200

    omer Degirmenci add pic

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 "use strict";
 
-const container = document.getElementsByClassName("container");
 const dropMenu = document.querySelector("#select-dropdown");
 const movieList = document.querySelector("#movieList"); //select
 movieList.addEventListener("change", async event => {
 	const apiKey = "d17f780";
 	const movieUrl = `https://www.omdbapi.com/?apikey=${apiKey}&t=${event.target.value}`;
+
 	const response = await fetch(movieUrl);
-    const movieData = await response.json();
-	const title = document.createElement("p");
-	title.innerText = movieData.title;
-	//container.appendChild(title);
-	console.log(movieData);
+	const movieData = await response.json();
+	let poster = document.createElement("img");
+	const imageUrl = movieData.Poster;
+	poster.src = imageUrl;
+	const posterDiv = document.getElementById("posterDiv");
+	posterDiv.innerHTML = "";
+	posterDiv.appendChild(poster);
 });
