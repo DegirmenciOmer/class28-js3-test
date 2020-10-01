3fb79bc0b17e77ae224a00439d23b3da9030c657
Omer Degirmenci <rumidegirmenci0@gmail.com>
2020-10-01 13:01:20 +0200

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict'
 
-/*
-    Put your JavaScript here
-*/
+const movieUrl = `https://www.omdbapi.com/?apikey=d17f780&`;
+
+const fetchJSON = async url => {
+    try {
+        const response = await fetch(url);
+        console.log(response);
+        const parsedData = await response.json();
+        console.log(parsedData);
+        console.log(parsedData.Title, parsedData.Plot, parsedData.Poster);
+        const dropMenu = document.querySelector('#select-dropdown');
+        console.log(dropMenu);
+        const movieList = document.querySelector('#movieList')//select
+       
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+
+window.onload = fetchJSON(movieUrl);
+
+
