2a50bdf888de373a4c36314d175da7c52d48e543
Omer Degirmenci <rumidegirmenci0@gmail.com>
2020-10-05 22:18:16 +0200

    add title

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 "use strict";
 
-const dropMenu = document.querySelector("#select-dropdown");
-const movieList = document.querySelector("#movieList"); //select
-movieList.addEventListener("change", async event => {
-	const apiKey = "d17f780";
-	const movieUrl = `https://www.omdbapi.com/?apikey=${apiKey}&t=${event.target.value}`;
+const fetchMovies = () => {
+	const movieList = document.querySelector("#movieList"); //select
+	movieList.addEventListener("change", async event => {
+		const movieUrl = `https://www.omdbapi.com/?apikey=d17f780&t=${event.target.value}`;
+		const response = await fetch(movieUrl);
+		const movieData = await response.json();
+		console.log(movieData);
+		renderDom(movieData);
+	});
+}
 
-	const response = await fetch(movieUrl);
-	const movieData = await response.json();
-	let poster = document.createElement("img");
-	const imageUrl = movieData.Poster;
-	poster.src = imageUrl;
+const renderDom = (data) => {
 	const posterDiv = document.getElementById("posterDiv");
 	posterDiv.innerHTML = "";
+	let poster = document.createElement("img");
+	poster.src = data.Poster;
+	let title = document.createElement("h1");
+	console.log(title);
+	title.innerHTML = data.Title;
+	posterDiv.appendChild(title);
 	posterDiv.appendChild(poster);
-});
+	const moviePlot = document.createElement('p');
+	moviePlot.innerHTML = '';
+	posterDiv.appendChild(moviePlot);
+	moviePlot.innerHTML = `${data.Plot}`;
+}
+
+window.onload = fetchMovies();
